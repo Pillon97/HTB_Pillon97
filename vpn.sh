6c182ae2631e445ef09da8a87fdce1b7fe958158
root <root@Maxilenovo.>
2024-12-19 17:32:55 +0100

    vpn

--- vpn.sh
+++ vpn.sh
@@ -1,17 +1,44 @@
 echo "Valassz ki melyik vpn-re szeretnl csatlakozni:"
 echo "1. Academy"
 echo "2. Lab"
+echo "3. exit"
 
-read -p "Vlaszd ki a szamot:" valasz
+read -p "Vlaszd ki a szamot:" vpn1
 
-case $valasz in
+case $vpn1 in
 	1)
-		vpn=academy-regular.ovpn
+		vpn=academy
 	;;
 	2)
-		vpn=lab_Pillon97.ovpn
+		vpn=lab
+	;;
+	3)
+		exit
+	;;
+	*)
+		echo "Rossz karaktert adtál meg!"
+esac
+echo "Válassz protocolt:"
+echo  "1. tcp"
+echo "2. udp"
+echo "3. exit"
+
+read -p "Válaszd ki a számot:" vpn2
+case $vpn2 in
+	1)
+		prot=tcp
+	;;
+	2)
+		prot=udp
+	;;
+	3)
+		exit
+	;;
+	*)
+		echo "Rossz karakter adtál meg!"
+		exit
 	;;
 esac
 
 
-sudo openvpn $vpn
+sudo openvpn "$vpn-$prot.ovpn"
